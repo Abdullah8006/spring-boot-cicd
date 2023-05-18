89b64ef26c62c0551d875387cc6dbb4dbed979d8
abd <abdullaha@phelix.ai>
2023-05-18 16:19:34 +0530

    Updated the repo for handling user ids

--- src/main/java/com/abd/cicd/contoller/UserController.java
+++ src/main/java/com/abd/cicd/contoller/UserController.java
@@ -3,6 +3,7 @@ package com.abd.cicd.contoller;
 import com.abd.cicd.model.UserModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -13,9 +14,13 @@ import java.util.List;
 public class UserController {
 
     @GetMapping()
-    public List<UserModel> getUsers() {
-        UserModel user = new UserModel("Abdullah", 34, "Software Developer");
-
+    public List<UserModel> getUsers(@RequestParam(required = false) Integer id) {
+        UserModel user;
+        if(id != null){
+            user = new UserModel("Inam", id, "Software Developer");
+        } else {
+            user = new UserModel("Abdullah", 37, "Software Developer");
+        }
         return Arrays.asList(user);
     }
 }
