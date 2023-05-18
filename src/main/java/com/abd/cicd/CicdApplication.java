01e80ac3ce61ef419bed8b46c7c420e7d2d08c9b
Abdullah Khan <abeiy4u@gmail.com>
2023-05-18 16:25:00 +0530

    Update CicdApplication.java

--- src/main/java/com/abd/cicd/CicdApplication.java
+++ src/main/java/com/abd/cicd/CicdApplication.java
@@ -3,6 +3,9 @@ package com.abd.cicd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+* This is test multiline comment
+*/
 @SpringBootApplication
 public class CicdApplication {
 
